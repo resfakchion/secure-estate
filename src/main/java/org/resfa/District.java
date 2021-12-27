64ed78b5c4eb07ae36ac60c5e8696993f64c6e97
k.litovchenko <klitovchenko@bivgroup.ru>
2021-12-27 10:03:29 +0300

    Small update

---
+++ src/main/java/org/resfa/District.java
@@ -0,0 +1,32 @@
+package org.resfa;
+
+public enum District {
+    Admiralteisky("admiralteisky.txt"),
+    Centralnyj("centralnyj.txt"),
+    Frunzensky("frunzensky.txt"),
+    Kalininsky("kalininsky.txt"),
+    Kirovsky("kirovsky.txt"),
+    Kolpinsky("kolpinsky.txt"),
+    Krasnogvardesky("krasnogvardesky.txt"),
+    Krasnoselsky("krasnoselsky.txt"),
+    Kronstadtsky("kronstadtsky.txt"),
+    Kurortnyj("kurortnyj.txt"),
+    Moskovsky("moskovsky.txt"),
+    Nevsky("nevsky.txt"),
+    Petrodvorcovyj("petrodvorcovyj.txt"),
+    Petrogradsky("petrogradsky.txt"),
+    Primorsky("primorsky.txt"),
+    Pushkinsky("pushkinsky.txt"),
+    Vasileostrovsky("vasileostrovsky.txt"),
+    Vyborgsky("vyborgsky.txt");
+
+    private String name;
+
+    District(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
