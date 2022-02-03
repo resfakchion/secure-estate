23dee15d60b4ff921b3f3be4e620275bd50632cb
Kirill <resfakchion@yandex.ru>
2022-02-03 11:37:11 +0300

    Last updates

--- src/main/java/org/resfa/service/SecureRealEstateResource.java
+++ src/main/java/org/resfa/service/SecureRealEstateResource.java
@@ -2,9 +2,9 @@ package org.resfa.service;
 
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
-import org.resfa.request.SecureRealEstateRequest;
 import org.resfa.pojo.District;
 import org.resfa.pojo.ParserResponse;
+import org.resfa.request.SecureRealEstateRequest;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -16,7 +16,6 @@ import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -26,7 +25,7 @@ public class SecureRealEstateResource {
     @Inject
     public InparseService inparseService;
 
-    @Path("/all")
+    @Path("/filter/all")
     @Produces(MediaType.APPLICATION_JSON)
     @GET
     public Response getAllEstate(SecureRealEstateRequest request) throws IOException, InterruptedException {
@@ -47,25 +46,38 @@ public class SecureRealEstateResource {
         return Response.ok().type("application/json").entity(response).build();
     }
 
-        @Path("/districts")
-        @Produces(MediaType.APPLICATION_JSON)
-        @GET
-        public Response getDistricts () throws IOException {
-            List<String> collect = Arrays.stream(District.values()).map(x -> x.getNumber() + " - " + x.getName()).collect(Collectors.toList());
-            return Response.ok().type("application/json").entity(collect).build();
-        }
-
-        @POST
-        @Path("/{id}")
-        @Produces(MediaType.APPLICATION_JSON)
-        @Consumes(MediaType.APPLICATION_JSON)
-        public Response getOptionalEstate (@PathParam String id, SecureRealEstateRequest request) throws Exception {
-            District district = Arrays.stream(District.values())
-                    .filter(x -> x.getNumber() == Integer.parseInt(id))
-                    .findFirst()
-                    .orElseThrow();
-            ParserResponse data = inparseService.optionalRequest(request, district);
-            return Response.ok().type("application/json").entity(data.data).build();
-        }
-
+    @Path("/districts")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    public Response getDistricts() throws IOException {
+        List<String> collect = Arrays.stream(District.values()).map(x -> x.getNumber() + " - " + x.getName()).collect(Collectors.toList());
+        return Response.ok().type("application/json").entity(collect).build();
     }
+
+    @POST
+    @Path("/district/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getOptionalEstate(@PathParam String id, SecureRealEstateRequest request) throws Exception {
+        District district = Arrays.stream(District.values())
+                .filter(x -> x.getNumber() == Integer.parseInt(id))
+                .findFirst()
+                .orElseThrow();
+        ParserResponse data = inparseService.optionalRequest(request, district);
+        return Response.ok().type("application/json").entity(data.data).build();
+    }
+
+    @GET
+    @Path("/district//{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getOptionalEstate(@PathParam String id) throws Exception {
+        District district = Arrays.stream(District.values())
+                .filter(x -> x.getNumber() == Integer.parseInt(id))
+                .findFirst()
+                .orElseThrow();
+        ParserResponse data = inparseService.getData(district);
+        return Response.ok().type("application/json").entity(data.data).build();
+    }
+
+}
